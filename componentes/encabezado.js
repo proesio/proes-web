99aeaf071516abd40e5ac747beeae52e6011751a
Juan Felipe Valencia Murillo <juanfe0245@gmail.com>
2020-08-24 22:34:48 -0500

    Redirección de enlace desarrollador a una página externa.

--- componentes/encabezado.js
+++ componentes/encabezado.js
@@ -1,40 +1,40 @@
 //Desarrollado Por Juan Felipe Valencia Murillo.
-Vue.component('encabezado',{
-	data(){
-		return {
-			host:DOMINIO
-		}
-	},
-	mounted(){
-		this.seleccionarBotonCargado();
-	},
-	methods:{
-		seleccionarBotonCargado(){
-			if(location.href==this.host) this.asignarEstiloBoton(document.getElementById('inicio'));
-			if(location.href==this.host+'proyectos/') this.asignarEstiloBoton(document.getElementById('proyectos'));
-			if(location.href==this.host+'desarrollador/') this.asignarEstiloBoton(document.getElementById('desarrollador'));
-		},
-		asignarEstiloBoton(btn){
-			btn.style.color='#5BC800';
-			btn.style.borderTop='5px solid white';
-			btn.style.borderBottom='5px solid #5BC800';
-		}
-	},
-	template:`
-		<header>
-			<div class="logo">
-				<a :href="host"><img :src="host+'imagenes/proes-eslogan-transparente.png'" alt="ESLOGAN PROES" style="width:auto; height:50px; margin-top:10px;"/></a>
-			</div>
-			<div class="rutas">
-				<a :href="host"><button id="inicio"><i class="fas fa-home"></i> Inicio</button></a>
-				<a :href="host+'proyectos'"><button id="proyectos"><i class="fas fa-book"></i> Proyectos</button></a>
-				<a :href="host+'desarrollador'"><button id="desarrollador"><i class="far fa-user"></i> Desarrollador</button></a>
-			</div>
-		</header>
-	`
+Vue.component('encabezado', {
+  data: () => ({
+    host: DOMINIO,
+    autorWeb: AUTOR_WEB
+  }),
+  mounted(){
+    this.seleccionarBotonCargado();
+  },
+  methods:{
+    seleccionarBotonCargado(){
+      if(location.href==this.host) this.asignarEstiloBoton(document.getElementById('inicio'));
+      if(location.href==this.host+'proyectos/') this.asignarEstiloBoton(document.getElementById('proyectos'));
+      if(location.href==this.host+'desarrollador/') this.asignarEstiloBoton(document.getElementById('desarrollador'));
+    },
+    asignarEstiloBoton(btn){
+      btn.style.color='#5BC800';
+      btn.style.borderTop='5px solid white';
+      btn.style.borderBottom='5px solid #5BC800';
+    }
+  },
+  template:`
+    <header>
+      <div class="logo">
+        <a :href="host"><img :src="host+'imagenes/proes-eslogan-transparente.png'" alt="ESLOGAN PROES" style="width:auto; height:50px; margin-top:10px;"/></a>
+      </div>
+      <div class="rutas">
+        <a :href="host"><button id="inicio"><i class="fas fa-home"></i> Inicio</button></a>
+        <a :href="host+'proyectos'"><button id="proyectos"><i class="fas fa-book"></i> Proyectos</button></a>
+        <a :href="autorWeb" target="_blank"><button id="desarrollador"><i class="far fa-user"></i> Desarrollador</button></a>
+      </div>
+    </header>
+  `
 });
-function abrirModal(id_modal){
-	modal=document.getElementById(id_modal);
+
+function abrirModal(modalId){
+	modal=document.getElementById(modalId);
 	modal.style.display='block';
 	window.onclick=function(event){
 		if(event.target==modal){
@@ -42,7 +42,8 @@ function abrirModal(id_modal){
 		}
 	}
 }
-function cerrarModal(id_modal){
-	modal=document.getElementById(id_modal);
+
+function cerrarModal(modalId){
+	modal=document.getElementById(modalId);
 	modal.style.display='none';
 }
--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 //Desarrollado Por Juan Felipe Valencia Murillo.
-const DOMINIO=location.origin+'/';
-const PIPE_URL='https://pipe.proes.tk';
+const DOMINIO = `${location.origin}/`;
+const AUTOR_WEB = 'https://jfvalenciam.proes.tk';
+const PIPE_URL = 'https://pipe.proes.tk';
