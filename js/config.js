4b6a54dd2970633ebd3385d6edb364e2f4b0eac8
Juan Felipe Valencia Murillo <juanfe0245@gmail.com>
2020-09-08 21:51:34 -0500

    Implementación de proyecto escrud en la página de proyectos.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 //Desarrollado Por Juan Felipe Valencia Murillo.
 const DOMINIO = `${location.origin}/`;
 const AUTOR_WEB = 'https://jfvalenciam.proes.tk';
+const ESCRUD_URL = 'https://escrud.proes.tk';
 const PIPE_URL = 'https://pipe.proes.tk';
